d39547d91e9fd4ecd7a65c323015200109d0dd10
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-29 18:14:21 -0500

    created server skeleton

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World!" })
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`)
+});
+
