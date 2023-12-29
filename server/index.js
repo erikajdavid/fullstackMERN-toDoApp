e595675e05d95ccc260309d0ae386b1d8b01a98d
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-29 18:58:35 -0500

    imported routes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello World!" })
-});
+const router = require("./routes");
+app.use("/api", router)
 
 const port = 5000;
 
