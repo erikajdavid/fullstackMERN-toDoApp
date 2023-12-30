2281187fb7ae8f010c1c966b1731425a88a93fba
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-30 12:40:15 -0500

    created GET and POST MongoDB queries, tested routes with POSTMAN

--- server/routes.js
+++ server/routes.js
@@ -2,18 +2,36 @@
 //endpoints responsible for the the frontend talking to the backend
 
 const express = require("express");
-
 const router = express.Router();
+const { getConnectedClient } = require("./database");
+
+const getCollection = () => {
+    const client = getConnectedClient();
+    const collection = client.db("todosdb").collection("todos");
+    return collection;
+}
 
 // GET /todos
-router.get("/todos", (req, res) => {
-    res.status(200).json({ message: "GET REQUEST TO /api/todos" })
+router.get("/todos", async (req, res) => {
+    const collection = getCollection();
+    const todos = await collection.find({}).toArray();
+    res.status(200).json(todos)
 });
 
 // POST /todos
 // create  todo
-router.post("/todos", (req, res) => {
-    res.status(201).json({ message: "POST REQUEST TO /api/todos" })
+router.post("/todos", async (req, res) => {
+    const collection = getCollection();
+    //console.log(req.body);
+
+    const { todo } = req.body;
+
+    const newTodo = await collection.insertOne({ todo, status: false})
+    //false status means an incomplete task in our todo application
+
+
+
+    res.status(201).json({ todo, status: false, _id: newTodo.insertedId })
 });
 
 // PUT /todos/:id
