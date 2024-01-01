65e7af982bd719c4049ba7e4e322d4edd97bc4be
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-01 16:30:11 -0500

    basic form set up, display backend data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,79 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-  const [message, setMessage] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [content, setContent] = useState("");
 
   useEffect(() => {
     async function getTodos() {
       try {
-        const res = await fetch("/api/todos");
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const todos = await res.json();
-        setMessage(todos.message);
-
+         const res = await fetch("/api/todos");
+   
+         if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+         }
+   
+         const todos = await res.json();
+         setTodos(todos);
       } catch (error) {
-        console.error("Error fetching todos:", error);
+         console.error("Error fetching todos:", error);
       }
-    }
-  
+   }   
     getTodos();
   }, []);
   
+  const createNewTodo = async (e) => {
+    e.preventDefault();
+    if (content.length > 3) {
+      const res = await fetch("api/todos", {
+        method: "POST",
+        body: JSON.stringify({ todo: content }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const newTodo = await res.json();
+
+      setContent("");
+      setTodos([...todos, newTodo]);
+    }
+  }
   
   //keeping the dependency array empty means it's only going to run one time. 
   //we only want to get our todos one time. 
 
+  //pre tag instead of div makes the JSON data easier to read, better formatted.
+
   return (
     <main className="container">
-      <h1>Hello World!</h1>
-      {message && <p>{message}</p>}
+      <h1>To-do App</h1>
+      <form onSubmit={createNewTodo}>
+        <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="Enter a new todo"
+        required
+        />
+        <button type="submit">Create todo</button>
+      </form>
+      <div>
+        {(todos.length > 0) && 
+          todos.map((todo) => {
+            return(
+              <div key={todo._id}>
+                <p>{todo.todo}</p>
+              <div>
+                <button>
+                  {(todo.status ? "☑" : "☐")}
+                </button>
+              </div>
+            </div>
+            )
+          })
+        }
+      </div>
+      
     </main>
   );
 }
